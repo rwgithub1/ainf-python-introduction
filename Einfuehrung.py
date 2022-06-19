7ed956186dce14d4935d9332e9a844abeb38e90d
Prof. Dr. R. Wilke <robin.wilke@haw-hamburg.de>
2022-06-19 21:43:37 +0200

    erste eintraege

---
+++ Einfuehrung.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 19 21:30:38 2022
+
+@author: rwilke
+"""
+
+from math import *
+
+#%% Funktion definieren:
+def hello_world():
+  print("Hello World!")
+#%% Funktion aufrufen:
+hello_world()
+
+#%% Mathe-Skript
+
+a = sin(0.34*pi)
+b = cos(0.34*pi)
+c = a + b
+
+print(c) # Ausgabe des Ergebnisses
